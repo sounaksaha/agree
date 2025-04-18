a6371d4589dea3a7a088dbb6e44d5817387c964b
Sounak Saha <sounaksaha@virtualemployee.com>
2025-04-18 11:51:28 +0530

    login registration done

--- controllers/admin_controller/adminController.js
+++ controllers/admin_controller/adminController.js
@@ -1,19 +1,22 @@
-import jwt from 'jsonwebtoken';
-import Admin from '../../model/admin.js';
-
+import jwt from "jsonwebtoken";
+import Admin from "../../model/admin.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
 export const registerAdmin = async (req, res) => {
   try {
     const { email, password, role } = req.body;
 
     const existingAdmin = await Admin.findOne({ email });
-    if (existingAdmin) return res.status(400).json({ message: 'Admin already exists' });
+    if (existingAdmin)
+      return res.status(400).json(new ApiResponse(400, "Admin already exists"));
 
     const newAdmin = new Admin({ email, password, role });
     await newAdmin.save();
 
-    res.status(201).json({ message: 'Admin registered successfully' });
+    res
+      .status(201)
+      .json(new ApiResponse(201, "Admin registered successfully", newAdmin));
   } catch (error) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json(new ApiResponse(500, "Server error", error));
   }
 };
 
@@ -22,13 +25,18 @@ export const loginAdmin = async (req, res) => {
 
   const admin = await Admin.findOne({ email });
   if (!admin || !(await admin.matchPassword(password))) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+    return res.status(401).json(new ApiResponse(401, "Invalid credentials"));
   }
 
   const payload = { id: admin._id, role: admin.role };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
 
-  res.json({ token, admin: { email: admin.email, role: admin.role } });
+  res.status(200).json(
+    new ApiResponse(200, "Login Successfull", {
+      token,
+      admin: { email: admin.email, role: admin.role },
+    })
+  );
 };
 
 export const getProfile = (req, res) => {
