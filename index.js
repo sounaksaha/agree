e47d3b70f1213c17ef70238daca52ac8a51ad053
yashrajput0508 <yasharajput0508@gmail.com>
2025-04-20 00:19:38 +0530

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import passport from 'passport';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
-import passportConfig from './config/passport.js';
+import passportConfig from "./config/passport.js";
 //import bookRoutes from './routes/bookRoutes.js';
 
 dotenv.config();
@@ -14,13 +15,20 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 // Passport config
 passportConfig(passport);
 app.use(passport.initialize());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
